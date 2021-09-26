9907b9f0495500420173db0bc0e2765d8d64c781
samuel <samuelreyesiglesias@gmail.com>
2021-09-25 21:31:13 -0700

    WEB API

---
+++ UI.WebApiClient/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using UI.WebApiClient.Models;
+
+namespace UI.WebApiClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            //consuming the api.
+
+            
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44351/Api/Simplest/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+            HttpResponseMessage Response = await Client.GetAsync("Index"); 
+            string StringResult = "";
+
+
+            if (Response.IsSuccessStatusCode)
+            {
+               StringResult= Response.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                StringResult = "Error getting the data";
+            }
+            return View(model:StringResult);
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApi/Controllers/SimplestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("Api/Simplest")]
+    public class SimplestController : Controller
+    {
+        [Produces("text/plain")]
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var StringContent = "Hello World  , 2nd example, easy example";
+                return Ok(StringContent);
+            }
+            catch (Exception Ex)
+            {
+                return Ok(BadRequest().ToString());
+            }
+        }
+    }
+}
